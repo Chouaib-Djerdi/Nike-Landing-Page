4f0454b1edcc6fbeb5a78c131f3fea987c2e9874
Chouaib-Djerdi <djerdichouaib@gmail.com>
2023-11-06 19:54:52 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+
+const App = () => (
+  <main className="relative">
+    Nav
+    <section className="xl:padding-1 wide-padding-r padding-b">
+      Hero
+    </section>
+    <section className="padding"></section>
+  </main>
+)
+
+
+export default App
